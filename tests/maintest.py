0874117eab86f0b2240842d2a0105c581e8f5d0b
juan <krazybean1@yahoo.com>
2014-08-04 07:33:48 +0000

    adding response test

--- tests/maintest.py
+++ tests/maintest.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import unittest
+import commands
 sys.path.insert(0,os.path.abspath(__file__+"/../.."))
 from app import main
 
@@ -14,6 +15,11 @@ class TestMain(unittest.TestCase):
     def test_randnum(self):
         m = main.randnum(6, 'num')
         self.assertTrue(bool(len(m) == 6))
+
+    def test_response(self):
+        commandx = "curl -s localhost:9003 | grep 'Random-as-a-service'"
+        reval = commands.getoutput(commandx)
+        self.assertTrue(bool(len(reval.split('\n')) == 2))
 
 if __name__=='__main__':
     unittest.main()
